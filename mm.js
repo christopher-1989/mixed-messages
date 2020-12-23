90d8f1fbb00ba90b675127b14474fcd0aaa7fce0
christopher-1989 <64772696+christopher-1989@users.noreply.github.com>
2020-12-23 12:52:03 +1100

    Sentence stems and keywords included. js file runs without error. Next phase is to include random selection of keywords

--- mm.js
+++ mm.js
@@ -1,10 +1,30 @@
+
 db = {
+    word1: "", 
+    word2: "",
     sentenceStems: [
-        "stem1",
-        "stem2"
+        `For my next trick, I will pull ${this.word1} out of ${this.word2}.`,
+        `I never truly understood ${this.word1} until I encountered ${this.word2}.`,
+        `${this.word1} is a slippery slope that leads to ${this.word2}.`,
+        `Michael Bay's new three-hour action epic pits ${this.word1} against ${this.word2}.`,
+        `They said we were crazy. They said we couldn't put ${this.word1} inside of ${this.word2}. They were wrong.`,
+        `In M. Night Shyamalan's new movie, Bruce Willis discovers that ${this.word1} had really been ${this.word2} all along.`
     ],
     keywords: [
-        ["word1", "word2"],
-        ["word1", "word2"]
+        "my soul",
+        "BATMAN",
+        "Barack Obama",
+        "poverty",
+        "millions of cane toads",
+        "sweet, sweet vengeance",
+        "liberals",
+        "being rich",
+        "powerful thighs",
+        "teenage pregnancy",
+        "white privilege",
+        "Ghandi",
+        "a brain tumour"
     ]
 }
+
+console.log(db.keywords)
